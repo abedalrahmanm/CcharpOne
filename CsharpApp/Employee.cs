a8b55abe46e08f46b06aec05e54d58003cf62e99
ABD <abedalrahman.mashgbeh@gmail.com>
2024-09-26 17:08:17 +0300

    Add project files.

---
+++ CsharpApp/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpApp
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime HiringDate { get; set; }
+        public decimal Salary { get; set; }
+        public decimal ssc { get; set; }
+
+        public decimal sscfunction()
+        {
+             ssc = Salary * Convert.ToDecimal( 0.075) ;
+            return ssc;
+        }
+    }
+}
---
+++ CsharpApp/Program.cs
@@ -0,0 +1,95 @@
+﻿//string fName = Console.ReadLine();
+
+//string mName = Console.ReadLine(), lName = Console.ReadLine(); 
+//int age =Convert.ToInt32( Console.ReadLine());
+
+
+//string fullName = fName + " "+ mName + " " + lName;
+
+//Console.WriteLine("full name: " + fullName + "his age: "+age.ToString());
+
+//using CsharpApp;
+
+//string sName = Console.ReadLine();
+
+//int sum = 0;
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.WriteLine($"please enter mark{i+1}!");
+//    int mark = Convert.ToInt32(Console.ReadLine());
+//    sum += mark;
+//}
+
+
+
+
+//int m1 = Convert.ToInt32(Console.ReadLine()),
+//    m2 = Convert.ToInt32(Console.ReadLine()),
+//    m3 = Convert.ToInt32(Console.ReadLine());
+
+//Student student = new Student();
+//student.StudentName = sName;
+//student.mark1 = m1;
+//student.mark2 = m2;
+//student.mark3 = m3;
+
+//Console.WriteLine(student.GetAvgName("abd",90,80,100));
+
+
+
+using CsharpApp;
+using System.Runtime.CompilerServices;
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee
+    {
+        id = 1,
+        name="ALI",
+        HiringDate = DateTime.Now,
+        Salary = 1000
+    },new Employee
+    {
+        id = 2,
+        name="ahmad",
+        HiringDate = DateTime.Now,
+        Salary = 500
+    },new Employee
+    {
+        id = 3,
+        name="omar",
+        HiringDate = DateTime.Now,
+        Salary = 800
+    }
+};
+
+Console.WriteLine("id \tname\tHiring Date\t salary");
+Console.WriteLine("-- \t----\t-----------\t ------");
+
+for (int i = 4; i < 7; i++)
+{
+    employees.Add(new Employee
+    {
+        id = Convert.ToInt32(Console.ReadLine()),
+        name = Console.ReadLine(),
+        HiringDate = DateTime.Now,
+        Salary = Convert.ToInt32(Console.ReadLine())
+    });
+
+
+
+
+}
+
+foreach (Employee emp in employees)
+{
+    Console.WriteLine($"{emp.id} \t {emp.name} \t {emp.HiringDate.ToShortDateString()} \t {emp.Salary}\t {emp.ssc} \t {emp.sscfunction()}");
+
+    //best practise
+    //make an obj then emp.Add(obj)
+}
+
+//employees[0].sscfunction(employees[0].Salary);
+
+
+
